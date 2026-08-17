6ee6c85e167ec76fa2ff3fb3f413ae48c41c118d
hyphen-hyphen-hyphen-hyphen <150187776+hyphen-hyphen-hyphen-hyphen@users.noreply.github.com>
2026-08-17 23:11:26 +0200

    fixed small errors
    thats why you test things before pushing

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 orig=$(pwd)
-dir=$(mktmp -d)
+dir=$(mktemp -d)
 cd "${dir}"
 git clone https://github.com/hyphen-hyphen-hyphen-hyphen/scout.git
 cd scout
 make
 sudo make install
 cd "${orig}"
-rm "${dir}"
+rm -rf "${dir}"
